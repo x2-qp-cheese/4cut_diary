f7056e59a575637752a9c6d95537ff8908cfa78d
GEONWOO PARK <pgw053024@naver.com>
2024-03-18 03:55:01 +0900

    fixed: resized slider

--- lib/userpage.dart
+++ lib/userpage.dart
@@ -2,9 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+
 class Userpage extends StatefulWidget {
   const Userpage({Key? key}) : super(key: key);
-
   @override
   State<Userpage> createState() => _UserpageState();
 }
@@ -14,46 +14,48 @@ class _UserpageState extends State<Userpage> {
     'assets/img/tmp1.png',
     'assets/img/tmp2.jpg',
     'assets/img/tmp3.png',
-    // Add all your image paths here
   ];
 
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
-    double screenWidth = MediaQuery.of(context).size.width;
+    double screenWidth = MediaQuery.of(context).size.width; // 4:3 비율에 따라 높이 계산
     return CupertinoPageScaffold(
       child: Align(
         child: Container(
           color: Colors.black,
-          width: screenWidth, 
-          height: screenHeight / 3.5,
-          child: slider(imagePaths),
+          width: screenWidth,
+          height: screenHeight/2,
+          child: slider(imagePaths, screenWidth, screenHeight/2),
         ),
       ),
     );
   }
 
-  Widget slider(List<String> images) {
+  Widget slider(List<String> images, double sliderWidth,  double sliderHeight) {
     return CarouselSlider(
       items: images.map((imagePath) {
         return Container(
           margin: EdgeInsets.all(2),
+          
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(30),
             color: Colors.orange, // Box color
           ),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(30),
-            child: Image.asset(
-              width: 300,
-              height: 400,
-              imagePath,
-              fit: BoxFit.contain,
-            ),
+          child: Center(
+            child: ClipRRect(
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.fitHeight,
+              ),
           ),
+          )
         );
       }).toList(),
       options: CarouselOptions(
+        height: sliderHeight/1.2,
+        aspectRatio: 3/4,
+        viewportFraction: 0.8,
         autoPlay: true,
         enlargeCenterPage: true,
         initialPage: 0,
